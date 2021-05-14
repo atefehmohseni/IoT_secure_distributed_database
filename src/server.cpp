be2801357f5621d0a975cea261d08d8213073cfe
Nicola Ruaro <ruaronicola@gmail.com>
2021-05-14 02:20:45 -0700

    set up server, separate database

--- src/server.cpp
+++ src/server.cpp
@@ -7,37 +7,48 @@ using namespace std;
 
 class IServer {
     public:
-    virtual optional<string> read_query(string key) = 0;
-    virtual void write_query(string key, string value) = 0;
-
+        IServer(){}
 };
 
-class Server: public IServer, public DataBase {
+class Server: public IServer {
     public:
         httplib::Server *http_server;
-        Server(){
-            this-> http_server = new httplib::Server;
+        DataBase *database;
+        Server() {
+            this->http_server = new httplib::Server;
+            this->database = new DataBase;
 
-            // set up routes for read/write query
-            
-            //this-> http_server.listen("0.0.0.0", 4444);
+            DEBUG("Setting up /read endpoint");
+            this->http_server->Get("/read", [&](const httplib::Request &req, httplib::Response &res) {
+                if (req.has_param("key")) {
+                    string key = req.get_param_value("key");
+                    optional<string> value = this->database->read(key);
+                    res.set_content(value->c_str(), "text/plain");
+                } else {
+                    res.set_content("", "text/plain");
+                }
+            });
+
+            DEBUG("Setting up /write endpoint");
+            this->http_server->Get("/write", [&](const httplib::Request &req, httplib::Response &res) {
+                if (req.has_param("key") && req.has_param("value")) {
+                    string key = req.get_param_value("key");
+                    string value = req.get_param_value("value");
+                    this->database->write(key, value);
+                    res.set_content("success", "text/plain");
+                } else {
+                    res.set_content("failure", "text/plain");
+                }
+            });
+
+            DEBUG("Listening on 0.0.0.0:4444");
+            this-> http_server->listen("0.0.0.0", 4444);
         }
-        optional<string> read_query(string key);
-        void write_query(string key, string value);
 
 };
-
-optional<string> Server::read_query(string key) {
-    optional<string> value = DataBase::read(key);
-    return value;
-}
-
-void Server::write_query(string key, string value) {
-    DataBase::write(key,value);
-}
 
 
 int main() {
     DEBUG("Starting a Server instance");
-    Server *server = new Server();
+    Server *server = new Server;
 }
