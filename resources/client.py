407202fe2d5cc9a845feb8943d5371eb4dd0fd54
Nicola Ruaro <ruaronicola@gmail.com>
2021-05-20 20:03:20 -0700

    make ssl work in python client

--- resources/client.py
+++ resources/client.py
@@ -1,31 +1,48 @@
+import argparse
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+SSL_CERT_FILE = "./ssl.debug.crt"
+SSL_KEY_FILE = "./ssl.debug.key"
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 class Client:
-    def __init__(self):
+    def __init__(self, ssl=True):
         self.http_session = requests.Session()
-        self.base_url = 'http://localhost:4444'
+        # self.http_session.cert = SSL_CERT_FILE
+        self.http_session.verify = False
+
+        if ssl:
+            self.base_url = 'https://localhost:4444'
+        else:
+            self.base_url = 'http://localhost:4444'
 
     def read_query(self, key):
-        r = requests.get(f'{self.base_url}/get', params={'key': key})
+        r = self.http_session.get(f'{self.base_url}/get', params={'key': key})
 
         if r.status_code == 200:
             return r.text
 
     def write_query(self, key, value):
-        r = requests.get(f'{self.base_url}/put', params={'key': key, 'value': value})
+        r = self.http_session.get(f'{self.base_url}/put', params={'key': key, 'value': value})
 
         if r.status_code == 200:
             return r.text
 
     def delete_query(self, key):
-        r = requests.get(f'{self.base_url}/delete', params={'key': key})
+        r = self.http_session.get(f'{self.base_url}/delete', params={'key': key})
 
         if r.status_code == 200:
             return r.text
 
 if __name__ == '__main__':
-    client = Client()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--no-ssl', action='store_true', default=False,
+                        help='Disable SSL')
+    args = parser.parse_args()
+
+    client = Client(ssl=(not args.no_ssl))
 
     while True:
         print('r) read query  w) write query  d) delete query  q) quit\nPlease enter your choice: ')
