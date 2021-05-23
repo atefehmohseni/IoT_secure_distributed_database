1d2ee62903e4c9d0b7be66fc6597fc966e30495a
Nicola Ruaro <ruaronicola@gmail.com>
2021-05-23 16:04:33 -0700

    implement volatile local store (+secondary thread) for python client

--- resources/client.py
+++ resources/client.py
@@ -1,11 +1,26 @@
 import argparse
 import requests
+import time
+import threading
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 SSL_CERT_FILE = "./ssl.debug.crt"
 SSL_KEY_FILE = "./ssl.debug.key"
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+class LocalStore:
+    def __init__(self):
+        self.data = dict()
+
+    def read_record(self, key):
+        return self.data[key]
+
+    def write_record(self, key, value):
+        self.data[key] = value
+
+    def delete_record(self, key):
+        del self.data[key]
 
 class Client:
     def __init__(self, ssl=True):
@@ -14,10 +29,16 @@ class Client:
         # self.http_session.cert = SSL_CERT_FILE
         self.http_session.verify = False
 
+        self.action_queue = []
+        self.local_store = LocalStore()
+
         if ssl:
             self.base_url = 'https://localhost:4444'
         else:
             self.base_url = 'http://localhost:4444'
+
+        # start secondary thread
+        threading.Thread(target=Client.local_store_callable, args=(self, )).start()
 
     def read_query(self, key):
         r = self.http_session.get(f'{self.base_url}/get', params={'key': key})
@@ -26,16 +47,43 @@ class Client:
             return r.text
 
     def write_query(self, key, value):
-        r = self.http_session.get(f'{self.base_url}/put', params={'key': key, 'value': value})
+        self.action_queue.insert(0, ('put', key))
 
-        if r.status_code == 200:
-            return r.text
+        # write value to local value store
+        self.local_store.write_record(key, value)
 
     def delete_query(self, key):
-        r = self.http_session.get(f'{self.base_url}/delete', params={'key': key})
+        self.action_queue.insert(0, ('del', key))
 
-        if r.status_code == 200:
-            return r.text
+    @staticmethod
+    def local_store_callable(client):
+        while True:
+            print('running', client.action_queue)
+            # process action queue
+            while len(client.action_queue) > 0:
+                status = 500
+                type, key = client.action_queue[0]
+
+                if type == 'del':
+                    r = client.http_session.get(f'{client.base_url}/delete', params={'key': key})
+                    status = r.status_code
+                elif type == 'put':
+                    # read value from local store
+                    value = client.local_store.read_record(key)
+                    r = client.http_session.get(f'{client.base_url}/put', params={'key': key, 'value': value})
+                    status = r.status_code
+                    pass
+
+                # check status
+                if status == 200:
+                    client.action_queue.pop(0)
+                    if type == 'put':
+                        # delete key from local data store
+                        client.local_store.delete_record(key)
+                else:
+                    break
+
+            time.sleep(5)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
