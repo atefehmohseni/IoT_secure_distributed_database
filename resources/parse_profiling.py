351d29c55a1892f360954cf3da9b8772336c8bd0
Nicola Ruaro <ruaronicola@gmail.com>
2021-05-23 20:50:02 -0700

    add syscalls profiling

--- resources/parse_profiling.py
+++ resources/parse_profiling.py
@@ -1,12 +1,15 @@
 import ast
 import re
 
+# to actually run the profiling, use something like:
+# for i in {1..10}; do _=$(cat ../test/write_testcase.txt | /usr/bin/time -o ./pypy.profiling.txt --append -f "%e real,\t%U user,\t%S sys,\t%P CPU,\t%Mk max mem,\t%F major pagefaults,\t%R minor pagefaults" pypy3 client.py); done
+
 REGEX = '^(?P<real>\d+\.\d+) real,	(?P<user>\d+\.\d+) user,	(?P<sys>\d+\.\d+) sys,	(?P<cpu>\d+)% CPU,	(?P<maxmem>\d+)k max mem,	(?P<Mpagefaults>\d+) major pagefaults,	(?P<mpagefaults>\d+) minor pagefaults'
 regex = re.compile(REGEX)
 groups = list(regex.groupindex)
 
 print('-'*20+'\ncpython profiling\n'+'-'*20)
-with open('cpython.profiling.txt') as f:
+with open('profiling_data/cpython.profiling.txt') as f:
     total = {group:0 for group in groups}
     lines = f.readlines()
     for line in lines:
@@ -17,7 +20,7 @@ with open('cpython.profiling.txt') as f:
     print({k:round(v/len(lines),2) for k,v in total.items()})
 
 print('-'*20+'\npypy profiling\n'+'-'*20)
-with open('pypy.profiling.txt') as f:
+with open('profiling_data/pypy.profiling.txt') as f:
     total = {group:0 for group in groups}
     lines = f.readlines()
     for line in lines:
@@ -28,7 +31,7 @@ with open('pypy.profiling.txt') as f:
     print({k:round(v/len(lines),2) for k,v in total.items()})
 
 print('-'*20+'\ncpp profiling\n'+'-'*20)
-with open('cpp.profiling.txt') as f:
+with open('profiling_data/cpp.profiling.txt') as f:
     total = {group:0 for group in groups}
     lines = f.readlines()
     for line in lines:
